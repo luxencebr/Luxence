1bd56adb095f2aa009b22fc1a896251e5768d2b2
artpimentel <art.ol.pimentel@gmail.com>
2025-11-27 13:10:27 -0300

    Build Fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,28 +1,26 @@
-// import Link from "next/link";
+import Link from "next/link";
 
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 
-// import { aboutPosts } from "@/data/blog";
+export default function AboutPage() {
+  return (
+    <main className={styles.container}>
+      {/* <h1 className={styles.title}>Sobre Nós</h1>
 
-// export default function AboutPage() {
-//   return (
-//     <main className={styles.container}>
-//       <h1 className={styles.title}>Sobre Nós</h1>
-
-//       <div className={styles.postsFlex}>
-//         {aboutPosts.map((post) => (
-//           <article key={post.id} className={styles.post}>
-//             <h2>{post.title}</h2>
-//             <p>{post.content}</p>
-//             <small className={styles.date}>
-//               Publicado em {new Date(post.date).toLocaleDateString("pt-BR")}
-//             </small>
-//             <Link href="" className={styles.articleLink}>
-//               Leia mais
-//             </Link>
-//           </article>
-//         ))}
-//       </div>
-//     </main>
-//   );
-// }
+      <div className={styles.postsFlex}>
+        {aboutPosts.map((post) => (
+          <article key={post.id} className={styles.post}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <small className={styles.date}>
+              Publicado em {new Date(post.date).toLocaleDateString("pt-BR")}
+            </small>
+            <Link href="" className={styles.articleLink}>
+              Leia mais
+            </Link>
+          </article>
+        ))}
+      </div> */}
+    </main>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,16 @@
-// "use client";
+"use client";
 
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 
-// import allProducers from "@/data/producers";
-// import sliderData from "@/data/sliderData";
+import Slider from "@/components/Slider/Slider";
+import ProductsRow from "@/components/ProductRow/ProductRow";
 
-// import Slider from "@/components/Slider/Slider";
-// import ProductsRow from "@/components/ProductRow/ProductRow";
-
-// export default function HomePage() {
-//   const topProducers = allProducers.slice(0, 10);
-//   const newProducers = [...allProducers].sort((a, b) =>
-//     b.user.createdAt.toISOString().localeCompare(a.user.createdAt.toISOString())
-//   );
-
-//   return (
-//     <>
-//       <Slider slides={sliderData} className={styles.homeSlider} />
-//       <ProductsRow producers={newProducers} title="Novidades" />
-//       <ProductsRow producers={topProducers} title="Top Exence" highlight />
-//     </>
-//   );
-// }
+export default function HomePage() {
+  return (
+    <>
+      {/* <Slider slides={sliderData} className={styles.homeSlider} />
+      <ProductsRow producers={newProducers} title="Novidades" />
+      <ProductsRow producers={topProducers} title="Top Exence" highlight /> */}
+    </>
+  );
+}
