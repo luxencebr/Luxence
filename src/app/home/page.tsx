7695742b4da6462858ef3cbaac82a1170456ef7b
artpimentel <art.ol.pimentel@gmail.com>
2025-11-27 13:05:01 -0300

    Build Fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
-import allProducers from "@/data/producers";
-import sliderData from "@/data/sliderData";
+// import allProducers from "@/data/producers";
+// import sliderData from "@/data/sliderData";
 
-import Slider from "@/components/Slider/Slider";
-import ProductsRow from "@/components/ProductRow/ProductRow";
+// import Slider from "@/components/Slider/Slider";
+// import ProductsRow from "@/components/ProductRow/ProductRow";
 
-export default function HomePage() {
-  const topProducers = allProducers.slice(0, 10);
-  const newProducers = [...allProducers].sort((a, b) =>
-    b.user.createdAt.toISOString().localeCompare(a.user.createdAt.toISOString())
-  );
+// export default function HomePage() {
+//   const topProducers = allProducers.slice(0, 10);
+//   const newProducers = [...allProducers].sort((a, b) =>
+//     b.user.createdAt.toISOString().localeCompare(a.user.createdAt.toISOString())
+//   );
 
-  return (
-    <>
-      <Slider slides={sliderData} className={styles.homeSlider} />
-      <ProductsRow producers={newProducers} title="Novidades" />
-      <ProductsRow producers={topProducers} title="Top Exence" highlight />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Slider slides={sliderData} className={styles.homeSlider} />
+//       <ProductsRow producers={newProducers} title="Novidades" />
+//       <ProductsRow producers={topProducers} title="Top Exence" highlight />
+//     </>
+//   );
+// }
