922275622f35c366b66f6f5c1ffaa66d16e8c292
artpimentel <art.ol.pimentel@gmail.com>
2025-12-15 18:17:45 -0300

    Build Fix

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef, useState, useEffect } from "react";
-import styles from "./ProductInfo.module.css";
+import styles from "./ProductInfo.module.className";
 
 import { TbCoinFilled } from "react-icons/tb";
 import { HiLocationMarker } from "react-icons/hi";
